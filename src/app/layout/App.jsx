7f5c1a59c431eb16b2fe2702da894d616bd695b9
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-19 15:56:09 +0100

    Adds account table

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,33 +1,101 @@
 import React from "react";
+import AccountTable from "../../features/accounts/accountTable/AccountTable";
 import AddTransactionModal from "../../features/transactions/addTransactionModal/AddTransactionModal";
 import TransactionTable from "../../features/transactions/transactionTable/TransactionTable";
 
-const dummyData = [
-  {
-    id: 1,
-    from: "Income.Salary",
-    amount: 25_000,
-    currency: "SEK",
-    to: "Asset.Checking",
-    date: "2021-10-25",
-  },
-  {
-    id: 2,
-    from: "Asset.Checking",
-    amount: 5000,
-    currency: "SEK",
-    to: "Expense.Rent",
-    date: "2021-10-29",
-  },
-  {
-    id: 3,
-    from: "Asset.Checking",
-    amount: 1000,
-    currency: "SEK",
-    to: "Expense.Insurance",
-    date: "2021-10-30",
-  },
-];
+const dummyData = {
+  transactions: [
+    {
+      id: 1,
+      from: "Income.Salary",
+      amount: 25_000,
+      currency: "SEK",
+      to: "Asset.Checking",
+      date: "2021-10-25",
+    },
+    {
+      id: 2,
+      from: "Asset.Checking",
+      amount: 5000,
+      currency: "SEK",
+      to: "Expense.Rent",
+      date: "2021-10-29",
+    },
+    {
+      id: 3,
+      from: "Asset.Checking",
+      amount: 1000,
+      currency: "SEK",
+      to: "Expense.Insurance",
+      date: "2021-10-30",
+    },
+  ],
+  accounts: [
+    {
+      id: 1,
+      name: "Asset",
+      increaseBalanceOn: 0,
+      balance: 19000,
+      parentId: -1,
+    },
+    {
+      id: 7,
+      name: "Asset.Checking",
+      increaseBalanceOn: 0,
+      balance: 19000,
+      parentId: 1,
+    },
+    {
+      id: 2,
+      name: "Liability",
+      increaseBalanceOn: 1,
+      balance: 0,
+      parentId: -1,
+    },
+    {
+      id: 3,
+      name: "Income",
+      increaseBalanceOn: 1,
+      balance: 25000,
+      parentId: -1,
+    },
+    {
+      id: 6,
+      name: "Income.Salary",
+      increaseBalanceOn: 1,
+      balance: 25000,
+      parentId: 3,
+    },
+    {
+      id: 4,
+      name: "Expense",
+      increaseBalanceOn: 0,
+      balance: -6000,
+      parentId: -1,
+    },
+    {
+      id: 8,
+      name: "Expense.Rent",
+      increaseBalanceOn: 0,
+      balance: -5000,
+      parentId: 3,
+    },
+    {
+      id: 9,
+      name: "Expense.Insurance",
+      increaseBalanceOn: 0,
+      balance: -1000,
+      parentId: 3,
+    },
+    {
+      id: 5,
+      name: "Capital",
+      increaseBalanceOn: 1,
+      balance: 0,
+      parentId: -1,
+    },
+  ],
+};
 
 export default class App extends React.Component {
   constructor() {
@@ -60,7 +128,7 @@ export default class App extends React.Component {
         <h1>Reledger</h1>
 
         <TransactionTable
-          data={dummyData}
+          data={dummyData.transactions}
           onAddTransactionClicked={this.openModal}
         />
         <AddTransactionModal
@@ -68,6 +136,7 @@ export default class App extends React.Component {
           onModalSubmitClicked={this.onModalSubmit}
           onModalClosed={this.closeModal}
         />
+        <AccountTable data={dummyData.accounts} />
       </div>
     );
   }
