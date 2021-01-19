5db5f7f152ada7f98fd7a3e53dfbb51b4044bb3b
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-19 19:24:54 +0100

    Adds NavLinks to make nav bar functional

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Menu, Container, Icon, Button } from "semantic-ui-react";
 
 export const Navbar = (props) => (
   <Menu inverted fixed="top" color="blue">
     <Container>
-      <Menu.Item header>
+      <Menu.Item as={NavLink} to="/" exact>
         <Icon name="book" size="large" />
         Reledger
       </Menu.Item>
-      <Menu.Item name="Accounts" />
-      <Menu.Item name="Transactions" />
+      <Menu.Item as={NavLink} to="/accounts" name="Accounts" />
+      <Menu.Item as={NavLink} to="/transactions" name="Transactions" />
       <Menu.Item position="right">
         <Button positive onClick={props.onAddTransactionClicked}>
           Add New Transaction
