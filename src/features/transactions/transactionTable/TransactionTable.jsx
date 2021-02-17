7fbd77aadddffac4c227dda495bfe7760668e7ff
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-02-17 16:42:50 +0100

    Fix wrong field name

--- src/features/transactions/transactionTable/TransactionTable.jsx
+++ src/features/transactions/transactionTable/TransactionTable.jsx
@@ -12,9 +12,9 @@ export const TransactionTable = (props) => (
         </Table.HeaderCell>
       </Table.Row>
       <Table.Row>
-        <Table.HeaderCell>From</Table.HeaderCell>
+        <Table.HeaderCell>Credit (From)</Table.HeaderCell>
         <Table.HeaderCell>Amount</Table.HeaderCell>
-        <Table.HeaderCell>To</Table.HeaderCell>
+        <Table.HeaderCell>Debit (To)</Table.HeaderCell>
         <Table.HeaderCell>Date</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
@@ -22,11 +22,11 @@ export const TransactionTable = (props) => (
     <Table.Body>
       {props.data.map((transaction) => (
         <Table.Row key={transaction.id}>
-          <Table.Cell>{transaction.fromAccount.name}</Table.Cell>
+          <Table.Cell>{transaction.creditAccount.name}</Table.Cell>
           <Table.Cell>
             {transaction.amount} {transaction.currency}
           </Table.Cell>
-          <Table.Cell>{transaction.toAccount.name}</Table.Cell>
+          <Table.Cell>{transaction.debitAccount.name}</Table.Cell>
           <Table.Cell>{transaction.date}</Table.Cell>
         </Table.Row>
       ))}
